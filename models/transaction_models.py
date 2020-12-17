6fac8880936cd40dae25adad835a3fb77185f7a3
Edwinmantilla182 <edwin.jmantilla@gmail.com>
2020-12-16 22:59:19 -0500

    noveno commit

--- models/transaction_models.py
+++ models/transaction_models.py
@@ -8,10 +8,10 @@ class TransactionIn(BaseModel):
     expense: int
 
 class TransactionOut(BaseModel):
-    id_transaction: int
-    username: str
+    id_transaccion: int
+    usuario: str
     cuenta: str
-    date: datetime
-    income: int
-    expense: int
-    actual_balance: int
+    fecha: datetime
+    ingreso: int
+    egreso: int
+    balance: int
